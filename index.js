d953726d45c367405bf0731c3ea9d23a817b9be5
Ashutosh Kumar Tiwari <9895145@gmail.com>
2025-06-26 16:26:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/youtube',(req,res)=>{
+    res.send("<h2> chai or kbchode </h2>")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
